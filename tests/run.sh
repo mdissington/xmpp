c996697a094b07123929b8ffd57c2547de169827
zorn <zorn7@yandex.ru>
2024-04-16 08:09:09 +1000

    Improve run tests script

--- tests/run.sh
+++ tests/run.sh
@@ -2,4 +2,6 @@
 
 cd "$(dirname $0)/.."
 
-docker run --rm -it -v"$PWD:/app" -w/app php:5.6 ./vendor/bin/phpunit
+CMD=${1:-phpunit}
+
+docker run --rm -it -v"$PWD:/app" -w/app php:${PHP_VERSION:-5.6} ./vendor/bin/${CMD}
